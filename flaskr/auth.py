43088c026892786119aaa82bdb4fb11b8c0d07bc
bGZoCg <bgzocg@outlook.com>
2021-06-17 00:25:22 +0800

    add no-comments template, yes its worked.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -115,6 +115,3 @@ def login_required(view):
 
     return wrapped_view
 # This decorator returns a new view function that wraps the original view it’s applied to. The new function checks if a user is loaded and redirects to the login page otherwise. If a user is loaded the original view is called and continues normally. You’ll use this decorator when writing the blog views.
-
-
-
