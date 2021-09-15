77ad545beb22a8d53e391bdbcb6b7155d093a160
bGZoCg <bgzocg@outlook.com>
2021-09-15 22:34:46 +0800

    add finished work of gomoku and add bat script in windows

---
+++ script/Start Here in Windows.bat
@@ -0,0 +1,5 @@
+@echo off
+start C:\Users\15517\scoop\apps\vivaldi\current\Application\vivaldi.exe
+start C:\Users\15517\scoop\apps\yesplaymusic\current\YesPlayMusic.exe
+start C:\Users\15517\scoop\apps\logseq\current\Logseq.exe
+
