b09aa8c8bf4de22a8d3fc80ea14c16ed5ecd2c29
bGZoCg <bgzocg@outlook.com>
2022-03-23 21:13:53 +0800

    feat: add module proxy pattern

---
+++ tasks/design-pattern/src/proxyPattern/schoolGirl.java
@@ -0,0 +1,22 @@
+package proxyPattern;
+
+/* File Name: schoolGirl
+ * Author: @bGZo
+ * Created Time: 3/23/2022 21:00
+ * License: MIT
+ * Description:
+ */
+public class schoolGirl {
+    private String name;
+
+    public schoolGirl(){this.name="anony";}
+    public schoolGirl(String name){this.name = name;}
+
+//    public schoolGirl(schoolGirl mm) {
+//        this.name=mm.name;
+//    }
+
+    public String getName() {
+        return name;
+    }
+}
