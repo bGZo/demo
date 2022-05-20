a6c1dfb31958cb26b9b5f81c4cc4b98e3769ce4f
bGZoCg <bgzocg@outlook.com>
2022-05-20 13:59:26 +0800

    feat: add chrome ext demo

---
+++ tasks/spring/src/main/java/tacos/HomeController.java
@@ -0,0 +1,9 @@
+package tacos;
+/* File Name: HomeController
+ * Author: @bGZo
+ * Created Time: 5/17/2022 18:56
+ * License: MIT
+ * Description: 
+ */
+public class HomeController {
+}
---
+++ tasks/spring/src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,9 @@
+package tacos;
+/* File Name: HomeControllerTest
+ * Author: bGZo
+ * Created Time: 5/17/2022 21:14
+ * License: MIT
+ * Description: 
+ */
+public class HomeControllerTest {
+}
